4fcc07fd35407533370af2a58a1c37e202a32b1c
Emerson Recto <emersontrecto@gmail.com>
2025-05-07 19:44:16 -0400

    added cart message

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,17 +5,21 @@ import Item from "./Item";
 export default function Cart({ cart, addToCart, removeFromCart }) {
   return (
     <>
-      {cart.map((item) => {
-        return (
-          <div key={item.id} className="cartDiv">
-            <Item
-              item={item}
-              addToCart={addToCart}
-              removeFromCart={removeFromCart}
-            />
-          </div>
-        );
-      })}
+      {cart.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        cart.map((item) => {
+          return (
+            <div key={item.id} className="cartDiv">
+              <Item
+                item={item}
+                addToCart={addToCart}
+                removeFromCart={removeFromCart}
+              />
+            </div>
+          );
+        })
+      )}
     </>
   );
 }
